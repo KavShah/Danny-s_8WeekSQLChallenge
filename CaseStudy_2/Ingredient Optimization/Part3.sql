0197e73bffdbfd39602dec0de03695db50333818
Kevin Shah <shah.kav@husky.neu.edu>
2021-07-07 20:00:17 -0700

    Part3-Q4 completed

--- CaseStudy_2/Ingredient Optimization/Part3.sql
+++ CaseStudy_2/Ingredient Optimization/Part3.sql
@@ -55,76 +55,242 @@ order by Excluded desc;
 --	Meat Lovers - Extra Bacon
 --	Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
 
-insert into customer_orders
-  ("order_id", "customer_id", "pizza_id", "exclusions", "extras", "order_time")
-values
-  ('11', '106', '1', null, null, '20200113 18:05:02'),
-  ('12', '107', '1', '3', null, '20200114 19:00:52'),
-  ('13', '108', '1', null, '1', '20200116 23:00:23'),
-  ('13', '108', '1', '1, 4', '6, 9', '20200116 23:00:23');
+--insert into customer_orders
+--  ("order_id", "customer_id", "pizza_id", "exclusions", "extras", "order_time")
+--values
+--  ('11', '106', '1', null, null, '20200113 18:05:02'),
+--  ('12', '107', '1', '3', null, '20200114 19:00:52'),
+--  ('13', '108', '1', null, '1', '20200116 23:00:23'),
+--  ('13', '108', '1', '1, 4', '6, 9', '20200116 23:00:23');
 
-  delete from customer_orders where order_id in (11,12,13)
+--  delete from customer_orders where order_id in (11,12,13);
+
+-- Postgres Query
+WITH cte_cleaned_customer_orders AS (
+  SELECT
+    order_id,
+    customer_id,
+    pizza_id,
+    CASE
+      WHEN exclusions IN ('', 'null') THEN NULL
+      ELSE exclusions
+    end AS exclusions,
+    CASE
+      WHEN extras IN ('', 'null') THEN NULL
+      ELSE extras
+    end AS extras,
+    order_time,
+    ROW_NUMBER() OVER () AS original_row_number
+  FROM pizza_runner.customer_orders
+),
+-- when using the regexp_split_to_table function only records where there are
+-- non-null records remain so we will need to union them back in!
+cte_extras_exclusions AS (
+    SELECT
+      order_id,
+      customer_id,
+      pizza_id,
+      REGEXP_SPLIT_TO_TABLE(exclusions, '[,\s]+')::INTEGER AS exclusions_topping_id,
+      REGEXP_SPLIT_TO_TABLE(extras, '[,\s]+')::INTEGER AS extras_topping_id,
+      order_time,
+      original_row_number
+    FROM cte_cleaned_customer_orders
+  -- here we add back in the null extra/exclusion rows
+  -- does it make any difference if we use UNION or UNION ALL?
+  UNION
+    SELECT
+      order_id,
+      customer_id,
+      pizza_id,
+      NULL AS exclusions_topping_id,
+      NULL AS extras_topping_id,
+      order_time,
+      original_row_number
+    FROM cte_cleaned_customer_orders
+    WHERE exclusions IS NULL AND extras IS NULL
+),
+cte_complete_dataset AS (
+  SELECT
+    base.order_id,
+    base.customer_id,
+    base.pizza_id,
+    names.pizza_name,
+    base.order_time,
+    base.original_row_number,
+    STRING_AGG(exclusions.topping_name, ', ') AS exclusions,
+    STRING_AGG(extras.topping_name, ', ') AS extras
+  FROM cte_extras_exclusions AS base
+  INNER JOIN pizza_runner.pizza_names AS names
+    ON base.pizza_id = names.pizza_id
+  LEFT JOIN pizza_runner.pizza_toppings AS exclusions
+    ON base.exclusions_topping_id = exclusions.topping_id
+  LEFT JOIN pizza_runner.pizza_toppings AS extras
+    ON base.extras_topping_id = extras.topping_id
+  GROUP BY
+    base.order_id,
+    base.customer_id,
+    base.pizza_id,
+    names.pizza_name,
+    base.order_time,
+    base.original_row_number
+),
+cte_parsed_string_outputs AS (
+SELECT
+  order_id,
+  customer_id,
+  pizza_id,
+  order_time,
+  original_row_number,
+  pizza_name,
+  CASE WHEN exclusions IS NULL THEN '' ELSE ' - Exclude ' || exclusions end AS exclusions,
+  CASE WHEN extras IS NULL THEN '' ELSE ' - Extra ' || extras end AS extras
+FROM cte_complete_dataset
+),
+final_output AS (
+  SELECT
+    order_id,
+    customer_id,
+    pizza_id,
+    order_time,
+    original_row_number,
+    pizza_name || exclusions || extras AS order_item
+  FROM cte_parsed_string_outputs
+)
+SELECT
+  order_id,
+  customer_id,
+  pizza_id,
+  order_time,
+  order_item
+FROM final_output
+ORDER BY original_row_number;
+
 
 --Q5 Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any relevant ingredients
 --	For example: "Meat Lovers: 2xBacon, Beef, ... , Salami"
 
---drop table #test
-  select  * 
-  into #piztop
-  from #test2
- --CROSS APPLY STRING_SPLIT(exclusions, ',') 
- --CROSS APPLY STRING_SPLIT(extras, ',')
-CROSS APPLY STRING_SPLIT(toppings, ',')
-	select * from #test1--2
-  select * from #excl
-  select * from #ext
-  select * from #pizTop
+--Postgres Query
 
- select  stand.row_num, stand.customer_id, stand.order_id, stand.StandardToppings, ex.ExtraToppings, exc.ExcludeToppings,
- case when ex.ExtraToppings is null and exc.ExcludeToppings is null then stand.StandardToppings  else null  end as 'Overall'
- ----when  ex.ExtraToppings is null and exc.ExcludeToppings is not null then
- ----(
- --(select row_num, customer_id, order_id, string_agg(convert(varchar,topping_name), ', ') As StandardToppings from
- -- #test1 t1 join #piztop pt
- -- on t1.pizza_id=pt.pizza_id
- -- join pizza_toppings pt1 on pt.value=pt1.topping_id
- -- group by row_num, customer_id, order_id
- --) as stand
- -- left join
- -- (select row_num, customer_id, order_id, string_agg(convert(varchar,topping_name), ', ') As ExtraToppings from #ext e
- -- join pizza_toppings pt1 on e.value=pt1.topping_id
- --  group by row_num, customer_id, order_id) as ex on stand.Row_Num=ex.Row_Num
- 
- --)
- --)
- from
- ( select row_num, customer_id, order_id, string_agg(convert(varchar,topping_name), ', ') As StandardToppings from
-  #test1 t1 join #piztop pt
-  on t1.pizza_id=pt.pizza_id
-  join pizza_toppings pt1 on pt.value=pt1.topping_id
-  group by row_num, customer_id, order_id
- ) as stand
-  left join
-  (select row_num, customer_id, order_id, string_agg(convert(varchar,topping_name), ', ') As ExtraToppings from #ext e
-  join pizza_toppings pt1 on e.value=pt1.topping_id
-   group by row_num, customer_id, order_id) as ex on stand.Row_Num=ex.Row_Num 
-     left join
-  (select row_num, customer_id, order_id, string_agg(convert(varchar,topping_name), ', ') As ExcludeToppings from #excl e1
-  join pizza_toppings pt1 on e1.value=pt1.topping_id
-   group by row_num, customer_id, order_id) as exc on stand.Row_Num=exc.Row_Num
+WITH cte_cleaned_customer_orders AS (
+  SELECT
+    order_id,
+    customer_id,
+    pizza_id,
+    CASE
+      WHEN exclusions IN ('', 'null') THEN NULL
+      ELSE exclusions
+    END AS exclusions,
+    CASE
+      WHEN extras IN ('', 'null') THEN NULL
+      ELSE extras
+    END AS extras,
+    order_time,
+    ROW_NUMBER() OVER () AS original_row_number
+  FROM pizza_runner.customer_orders
+),
+-- split the toppings using our previous solution
+cte_regular_toppings AS (
+SELECT
+  pizza_id,
+  REGEXP_SPLIT_TO_TABLE(toppings, '[,\s]+')::INTEGER AS topping_id
+FROM pizza_runner.pizza_recipes
+),
+-- now we can should left join our regular toppings with all pizzas orders
+cte_base_toppings AS (
+  SELECT
+    cte_cleaned_customer_orders.order_id,
+    cte_cleaned_customer_orders.customer_id,
+    cte_cleaned_customer_orders.pizza_id,
+    cte_cleaned_customer_orders.order_time,
+    cte_cleaned_customer_orders.original_row_number,
+    cte_regular_toppings.topping_id
+  FROM cte_cleaned_customer_orders
+  LEFT JOIN cte_regular_toppings
+    ON cte_cleaned_customer_orders.pizza_id = cte_regular_toppings.pizza_id
+),
+-- now we can generate CTEs for exclusions and extras by the original row number
+cte_exclusions AS (
+  SELECT
+    order_id,
+    customer_id,
+    pizza_id,
+    order_time,
+    original_row_number,
+    REGEXP_SPLIT_TO_TABLE(exclusions, '[,\s]+')::INTEGER AS topping_id
+  FROM cte_cleaned_customer_orders
+  WHERE exclusions IS NOT NULL
+),
+cte_extras AS (
+  SELECT
+    order_id,
+    customer_id,
+    pizza_id,
+    order_time,
+    original_row_number,
+    REGEXP_SPLIT_TO_TABLE(extras, '[,\s]+')::INTEGER AS topping_id
+  FROM cte_cleaned_customer_orders
+  WHERE extras IS NOT NULL
+),
+-- now we can perform an except and a union all on the respective CTEs
+cte_combined_orders AS (
+  SELECT * FROM cte_base_toppings
+  EXCEPT
+  SELECT * FROM cte_exclusions
+  UNION ALL
+  SELECT * FROM cte_extras
+),
+-- aggregate the count of topping ID and join onto pizza toppings
+cte_joined_toppings AS (
+  SELECT
+    t1.order_id,
+    t1.customer_id,
+    t1.pizza_id,
+    t1.order_time,
+    t1.original_row_number,
+    t1.topping_id,
+    t2.pizza_name,
+    t3.topping_name,
+    COUNT(t1.*) AS topping_count
+  FROM cte_combined_orders AS t1
+  INNER JOIN pizza_runner.pizza_names AS t2
+    ON t1.pizza_id = t2.pizza_id
+  INNER JOIN pizza_runner.pizza_toppings AS t3
+    ON t1.topping_id = t3.topping_id
+  GROUP BY
+    t1.order_id,
+    t1.customer_id,
+    t1.pizza_id,
+    t1.order_time,
+    t1.original_row_number,
+    t1.topping_id,
+    t2.pizza_name,
+    t3.topping_name
+)
+SELECT
 
-  order by stand.row_num
+  order_id,
+  customer_id,
+  pizza_id,
+  order_time,
+  original_row_number,
+  pizza_name || ': ' ||
+  -- this logic is quite intense!
+   STRING_AGG(
+    CASE
+      WHEN topping_count > 1 THEN  topping_count || 'x ' || topping_name
+      ELSE  topping_name
+      END,
+    ', '
+  ) AS ingredients_list
+FROM cte_joined_toppings
+GROUP BY
+  order_id,
+  customer_id,
+  pizza_id,
+  order_time,
+  original_row_number,
+  pizza_name;
+
+
+  --Q6. What is the total quantity of each ingredient used in all delivered pizzas sorted by most frequent first?
   
-
-select t1.row_num, t1.customer_id, t1.order_id, string_agg(convert(varchar,topping_name), ', ') as exclude
- --, string_agg(convert(varchar,topping_name), ', ') As StandardToppings 
- from
-  #test1 t1 join #piztop pt
-  on t1.pizza_id=pt.pizza_id
-  join pizza_toppings pt1 on pt.value=pt1.topping_id
-  join #excl e
-  on t1.Row_Num=e.Row_Num --and e.pizza_id=pt.pizza_id
-  where pt.value != e.value
-   group by t1.row_num, t1.customer_id, t1.order_id
-
-
